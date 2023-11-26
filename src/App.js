7e7493af5a6d89ed29f2f6718750d2e2626f60e5
junyeokk <cjh4302@gmail.com>
2023-11-26 16:15:07 +0900

    feat: api front 코드

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
 
 function App() {
+    const baseUrl = "http://localhost:8080";
+  const [hello, setHello] = useState('')
+
+  useEffect(() => {
+    axios.get(baseUrl + '/api/hello')
+        .then(response => setHello(response.data))
+        .catch(error => console.log(error))
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        백엔드에서 가져온 데이터입니다 : {hello}
+      </div>
   );
 }
 
