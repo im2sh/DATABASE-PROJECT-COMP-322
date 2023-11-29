d010b14472572bf042e7f43beb1a923bf747652f
trialegg <trialegg@gmail.com>
2023-11-29 18:09:36 +0900

    Feat: "04-modify WriteDiaryPage.jsx"
    resizing layout

--- phase-4/togaether/src/main/frontend/src/pages/writediary/WriteDiaryPage.jsx
+++ phase-4/togaether/src/main/frontend/src/pages/writediary/WriteDiaryPage.jsx
@@ -1,6 +1,15 @@
 // WriteDiaryPage.jsx
 import React, { useState } from "react";
-import { Select, Textarea, VStack, Image, Box, FormControl } from '@chakra-ui/react';
+import {
+    Select,
+    Textarea,
+    VStack,
+    Image,
+    FormControl,
+    FormLabel,
+    Container,
+      Box
+  } from '@chakra-ui/react';
 import happyImage from '../../image/text=happy, size=L.png'; // Make sure the image path is correct
 
 const WriteDiaryPage = () => {
@@ -8,32 +17,38 @@ const WriteDiaryPage = () => {
   const [emotion, setEmotion] = useState('happy'); // Default emotion or could be ''
 
   return (
-    <VStack spacing={8} align="center" m={4}>
-      {/* Emotion Image */}
-      <Box boxSize="sm">
-        <Image src={happyImage} alt={emotion} />
-      </Box>
-      
-      {/* Emotion Dropdown */}
-      <FormControl>
-        <Select placeholder="Select emotion" value={emotion} onChange={(e) => setEmotion(e.target.value)}>
-          <option value="happy">Happy</option>
-          <option value="excited">Excited</option>
-          {/* Add more options as needed */}
-        </Select>
-      </FormControl>
+    <Container centerContent>
+        <VStack spacing={4} align="center" m={4}>
+        {/* Emotion Image */}
+        <Box boxSize="6.5625rem">
+            <Image src={happyImage} alt="Happy" boxSize="100%" objectFit="cover" />
+        </Box>
+        
+        {/* Emotion Dropdown */}
+        <FormControl id="emotion">
+            <FormLabel>행복해요</FormLabel>
+            <Select placeholder="감정을 선택해주세요" value={emotion} onChange={(e) => setEmotion(e.target.value)}>
+            <option value="happy">Happy</option>
+            <option value="excited">Excited</option>
+            {/* Add more options as needed */}
+            </Select>
+        </FormControl>
 
-      {/* Textarea for diary entry */}
-      <Textarea
-        placeholder="오늘 하루, 무슨 일이 있었나요? 자유롭게 작성해보세요."
-        size="md"
-        resize="none"
-        value={diaryText}
-        onChange={(e) => setDiaryText(e.target.value)}
-        minHeight="200px"
-        w="100%" // Set the width to 100% of the parent container
-      />
-    </VStack>
+        {/* Textarea for diary entry */}
+        <FormControl id="diary">
+            <FormLabel>오늘 하루, 반려견과 어떤 것들을 했나요?</FormLabel>
+            <Textarea
+                placeholder="오늘 하루, 무슨 일이 있었나요? 자유롭게 작성해보세요."
+                size="md"
+                resize="none"
+                value={diaryText}
+                onChange={(e) => setDiaryText(e.target.value)}
+                minHeight="200px"
+                w="100%" // Set the width to 100% of the parent container
+            />
+        </FormControl>
+        </VStack>
+    </Container>
   );
 };
 
