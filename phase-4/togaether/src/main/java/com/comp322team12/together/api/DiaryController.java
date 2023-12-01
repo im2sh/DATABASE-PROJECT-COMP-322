8ba3b382a0c55a3cc275067a611f47a48a6f18dc
im2shandyou@gmail.com <104557685+im2sh@users.noreply.github.com>
2023-12-01 11:26:15 +0900

    feat: 다이어리 생성 기능 추가

---
+++ phase-4/togaether/src/main/java/com/comp322team12/together/api/DiaryController.java
@@ -0,0 +1,48 @@
+package com.comp322team12.together.api;
+
+import com.comp322team12.together.dto.request.diary.DiaryCreateRequest;
+import com.comp322team12.together.dto.response.common.ResponseDto;
+import com.comp322team12.together.exception.place.InvalidPlaceException;
+import com.comp322team12.together.exception.user.InvalidUserException;
+import com.comp322team12.together.service.DiaryService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "Diary", description = "Diary 관련 API 입니다.")
+@RestController
+@RequiredArgsConstructor
+@CrossOrigin
+@RequestMapping(value = "/api/diary", produces = "application/json; charset=UTF-8")
+public class DiaryController {
+    private final DiaryService diaryService;
+
+    @Operation(
+            summary = "다이어리 생성",
+            description = "다이어리를 생성합니다."
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "다이어리 생성에 성공하였습니다."
+    )
+    @PostMapping("/create/{userId}/{placeId}")
+    public ResponseEntity<?> createDiary(@PathVariable Long userId, @PathVariable Long placeId, @RequestBody DiaryCreateRequest diaryCreateRequest) {
+        try {
+            diaryService.createDiary(userId, placeId, diaryCreateRequest);
+            return ResponseEntity.ok().body(new ResponseDto("다이어리 생성에 성공하였습니다."));
+        } catch (InvalidPlaceException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ResponseDto(e.getMessage()));
+        } catch (InvalidUserException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ResponseDto(e.getMessage()));
+        }
+    }
+}
